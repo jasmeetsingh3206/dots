3ae81e0756282d740fd85e709c82766a69cf67b0
Jasmeet Singh Sethi <jasmeet@sonartechnologies.com>
2025-01-17 14:33:39 +0530

    month view added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,106 @@
-import Image from "next/image";
+'use client';
+import { useEffect, useState } from 'react';
+import { DateTime } from 'luxon';
+import { AnimatePresence, motion } from 'framer-motion';
+
+const generateYearData = () => {
+	const startOfYear = DateTime.now().startOf('year');
+	const endOfYear = DateTime.now().endOf('year');
+
+	let yearData = [] as DateTime[];
+	for (let i = startOfYear; i <= endOfYear; i = i.plus({ days: 1 })) {
+		yearData.push(i);
+	}
+	return yearData;
+};
+
+const DotsGrid = ({ data, isMonthView }: { data: DateTime[]; isMonthView: boolean }) => {
+	return (
+		<motion.div
+			className={`grid justify-items-center  gap-3 transition-all ${
+				isMonthView
+					? 'grid-cols-7 sm:gap-4 sm:pt-8' // 7 columns for a month
+					: 'grid-cols-[repeat(16,minmax(0,1fr))] sm:grid-cols-[repeat(26,minmax(0,1fr))] sm:gap-x-2 sm:gap-y-8 sm:pt-6'
+			}`}>
+			{data.map((date) => (
+				<motion.div
+					key={date.toISO()} // Unique key
+					layout // Enables smooth transitions
+					transition={{ type: 'spring', damping: 15, stiffness: 100 }}
+					className='group relative cursor-pointer'>
+					<div className={`transition-all hover:scale-125 rounded-full bg-white ${isMonthView ? 'size-5 sm:size-6' : 'size-3 sm:size-4'} ${date < DateTime.now().minus({ days: 1 }) ? 'opacity-50' : ''} ${date.hasSame(DateTime.now(), 'day') ? '!bg-red-500 animate-pulse ' : ''}`}></div>
+					<motion.div
+						animate={{ rotate: 360 }}
+						className='hidden group-hover:block absolute z-50 shadow-2xl text-sm bg-zinc-900 text-white rounded left-5 min-w-36 p-2'>
+						{date.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY)}
+					</motion.div>
+				</motion.div>
+			))}
+		</motion.div>
+	);
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	const [currentDate, setCurrentDate] = useState<DateTime | null>(null);
+	const [yearData, setYearData] = useState<DateTime[]>([]);
+	const [viewMode, setViewMode] = useState<'year' | 'month'>('year');
+	const [remainingMode, setRemainingMode] = useState<'M1' | 'M2'>('M1');
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	useEffect(() => {
+		const now = DateTime.now();
+		setYearData(generateYearData());
+		setCurrentDate(now);
+	}, []);
+
+	const handleDateClick = () => {
+		setViewMode((prev) => (prev === 'year' ? 'month' : 'year'));
+	};
+
+	const handleRemainingButtonClick = () => {
+		setRemainingMode((prev) => (prev === 'M1' ? 'M2' : 'M1'));
+	};
+
+	if (!currentDate) return null;
+
+	// Filter data for current month
+	const filteredData = viewMode === 'month' ? yearData.filter((d) => d.hasSame(currentDate, 'month')) : yearData;
+
+	const daysLeft = currentDate.endOf('year').ordinal - currentDate.ordinal;
+	const percentageLeft = ((daysLeft / currentDate.endOf('year').ordinal) * 100).toFixed(1);
+
+	return (
+		<main className='bg-black h-screen font-mono'>
+			<div className='container pt-10 mx-auto bg-black'>
+				<DotsGrid
+					data={filteredData}
+					isMonthView={viewMode === 'month'}
+				/>
+			</div>
+			<div className='absolute w-screen  bottom-10 '>
+				<div className='container hover:bg-white hover:bg-opacity-5 py-3 rounded-xl mx-auto flex justify-between items-center text-white px-2'>
+					<span
+						className='cursor-pointer'
+						onClick={handleDateClick}>
+						{viewMode === 'year' ? currentDate.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY) : currentDate.toLocaleString({ month: 'long', year: 'numeric' })}
+					</span>
+
+					<button
+						onClick={handleRemainingButtonClick}
+						className='relative min-w-32 text-right'>
+						<AnimatePresence mode='wait'>
+							<motion.div
+								key={remainingMode}
+								transition={{ duration: 0.1 }}
+								initial={{ y: 10, opacity: 0 }}
+								animate={{ y: 0, opacity: 1 }}
+								exit={{ y: -10, opacity: 0 }}
+								className='absolute inset-0 flex items-center justify-center w-full h-full'>
+								{remainingMode === 'M1' ? `${daysLeft} Days left.` : `${percentageLeft}% left.`}
+							</motion.div>
+						</AnimatePresence>
+					</button>
+				</div>
+			</div>
+		</main>
+	);
 }
